a6e7bb07c6632a42443619fe245bbd55bfd98b34
ChapaGaming <159574595+ChapaGaming@users.noreply.github.com>
2024-02-09 22:58:34 +0300

    v 0.11
    add all in peshka

--- heroes.py
+++ heroes.py
@@ -8,24 +8,28 @@ class Thather():
         """look -> str, black -> bool, x,y -> int"""
         self.x = x
         self.y = y
-        self.look = look
         self.black = black
+        self.look = look
     def cord(self):
         return [self.x,self.y]
     def delete(self,board):
         """Заменяет предмет на ' ' """
         board[self.x][self.y] = ' '
 class Peshka(Thather):
-    def __init__(self,look,black,x,y):
+    def __init__(self,look,black,x,y,first = True):
         """look -> str, black -> bool, x,y -> int"""
         super().__init__(look,black,x,y)
+        self.first = first
     def move(self,board):
         """Возвращает нужные координаты для перестановки"""
-        self.places1 = [self.can_go(board)]
+        self.places1 = []
+        if self.can_go(board) != -1:
+            self.places1 = self.can_go(board)
         self.places2 = self.can_front(board)
         if self.places2 != -1:
             for pl in self.places2:
-                self.places1.append(pl)
+                if pl != -1:
+                    self.places1.append(pl)
         return self.places1
     def can_go(self,board):
         """Возвращает передние координаты
@@ -39,13 +43,15 @@ class Peshka(Thather):
         else:
             self.go = +1
 
-        if board[self.x][self.y + self.go] == ' ':
-
-            return self.y + self.go
-
-        else:
-
-            return -1
+        if board[self.y + self.go][self.x] == ' ':
+            self.rett = [[self.x,self.y + self.go]]
+            if self.first:
+                if board[self.y + 2*self.go][self.x] == ' ':
+                    self.rett.append([self.x,self.y + 2*self.go])
+                    return self.rett
+            else:
+                return self.rett
+        return -1
     def can_front(self,board):
         """Проверяет:
              ___пешка___
@@ -60,9 +66,9 @@ class Peshka(Thather):
         else:
             self.go = +1
         self.ret = -1
-        if board[self.x + 1][self.y + self.go] != ' ':
+        if board[self.y + self.go][self.x + 1] != ' ':
             self.ret = [self.x + 1,self.y + self.go]
-        if board[self.x - 1][self.y + self.go] != ' ':
+        if board[self.y + self.go][self.x - 1] != ' ':
 
             return [[self.x - 1,self.y + self.go],self.ret]
         else:
--- only_console.py
+++ only_console.py
@@ -1,5 +1,6 @@
 import heroes as h#собственный файл
 from time import sleep
+from colorama import *
 def delete(obj):
     """Заменяет предмет на ' ' """
     x = obj.x
@@ -49,21 +50,41 @@ class ChessBoard():
             for line in self.lines:
                 self.N = int(line.split('_')[1]) -1
                 self.board[self.N] = self.lines[line]
+    def chet(self,x):
+        if '.5' in str(x /2):
+            return False
+        return True
     def show(self):
         """показывает список"""
-        self.print = ''
+        #self.print = ' 1'
+        i = 1
+        print(" a b c d e f g h")
+        for line in self.board:
+            if self.chet(i):
+                print(str(i) + Back.BLACK + " "+line[0] + Back.WHITE + " "+line[1] + Back.BLACK + " "+line[2] + Back.WHITE + " "+line[3] + Back.BLACK + " "+line[4] + Back.WHITE + " "+line[5] + Back.BLACK + " "+line[6] + Back.WHITE + " "+line[7]+ Style.RESET_ALL)
+            else:
+                print(str(i) + Back.WHITE + " "+line[0] + Back.BLACK + " "+line[1] + Back.WHITE + " "+line[2] + Back.BLACK + " "+line[3] + Back.WHITE + " "+line[4] + Back.BLACK + " "+line[5] + Back.WHITE + " "+line[6] + Back.BLACK + " "+line[7] + Style.RESET_ALL )
+            i+=1
+        """Другой алгоритм
+
+
         for line in self.board:
             for simvol in line:
-                self.print += simvol
-                self.print += ' '
-            self.print += '\n'
+                if self.chet(n_sim):
+                    self.print += Fore.WHITE + simvol + ' ' + Style.RESET_ALL
+                else:
+                    self.print += Fore.BLACK + simvol + ' ' + Style.RESET_ALL
+            if i != 9:
+                self.print += '\n '+str(i)
+                i += 1
+            n_sim += 1
         print(self.print)
+        """
     def create_peshki(self):
         x = 1
         for x in range(8):# Чёрные пешки
             if x == 0: x = 1
-            self.p = h.Peshka('♟',True,x-1,7)
-            print(self.p.y,self.p.x)
+            self.p = h.Peshka('♟',True,x-1,6)
             self.board[self.p.y][self.p.x] = self.p.look
             self.peshki.append(self.p)
         y = 1
@@ -77,12 +98,19 @@ class ChessBoard():
             self.board[cord[1]][cord[0]] = p.look
 
 board = ChessBoard()
+if 1 == 1:
+#while True:
+    #of = input("Фигура для ходьбы: ")
+    #to = input("Куда поставить: ")
+    el = board.peshki[12]
+    el.look = Back.GREEN+ el.look
 
-while True:
-    of = input("Фигура для ходьбы: ")
-    to = input("Куда поставить: ")
-    print(board.peshki[1].move(board.board))
+    liste = el.move(board.board)
+    print(liste)
+    for c in liste:
+
+        board.board[c[1]][c[0]] = "✗"
     board.update()
     board.show()
-    if to == '':
-        break
+    #if to == '':
+    #    break
